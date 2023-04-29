9fbc180cc61da461c3df5bf4751e209a72d4ab66
Rafael Santos <rafaelnsantos1@gmail.com>
2023-04-29 15:22:39 -0300

    remove import

--- src/business-unit/jumbo/jumbo.module.ts
+++ src/business-unit/jumbo/jumbo.module.ts
@@ -3,7 +3,6 @@ import { JumboService } from './jumbo.service';
 import { JumboResolver } from './jumbo.resolver';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { JumboEntity } from './jumbo.entity';
-import { GrupoModule } from '../grupo/grupo.module';
 
 @Module({
   imports: [TypeOrmModule.forFeature([JumboEntity])],
